6e19dbd9b36ab6511bf698c6a31bf25aaef25d8d
gmoroz <12345hasek@gmail.com>
2023-02-03 19:31:09 +0300

    create boilerplate

---
+++ app/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
+from sqlalchemy.orm import sessionmaker
+
+from app.core.config import settings
+
+engine = create_engine(settings.DATABASE_URI, pool_pre_ping=True)
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base = declarative_base()
