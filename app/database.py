abfbb014f33108314c99b3c5207f28c9b52ff238
gmoroz <12345hasek@gmail.com>
2023-02-03 22:22:33 +0300

    configure db and places table

--- app/database.py
+++ app/database.py
@@ -1,11 +1,24 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base, declared_attr
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Table, Column, Integer, String, ARRAY, MetaData
+from databases import Database
 
 from app.core.config import settings
 
 engine = create_engine(settings.DATABASE_URI, pool_pre_ping=True)
-Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+metadata = MetaData()
 
+places = Table(
+    "places",
+    metadata,
+    Column("pk", Integer, primary_key=True),
+    Column("title", String(200)),
+    Column("description", String(2000)),
+    Column("picture_url", String(200)),
+    Column("price", Integer),
+    Column("country", String(100)),
+    Column("city", String(50)),
+    Column("features_on", ARRAY),
+    Column("features_on", ARRAY(String(100))),
+    Column("features_off", ARRAY(String(100))),
+)
 
-Base = declarative_base()
+database = Database(settings.DATABASE_URI)
