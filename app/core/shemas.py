1b224d3b2f148bb29e8ed2f4eb90f5d31a5d9ca4
gmoroz <12345hasek@gmail.com>
2023-02-04 10:38:32 +0300

    feat: add pydantic shemas

--- app/core/shemas.py
+++ app/core/shemas.py
@@ -1,5 +1,17 @@
 from pydantic import BaseModel
 
 
-class Place(BaseModel):
-    ...
+class PlaceBase(BaseModel):
+    pass  # need to refactor this soon
+
+
+class PlaceOutDetail(PlaceBase):
+    pk: int
+    title: str
+    description: str
+    picture_url: str
+    price: int
+    country: str
+    city: str
+    features_on: list[str]
+    features_off: list[str]
